f5c7ed430a61a05e6cff9f01b5a286bdfcf34631
Jamal <43446907+JamalLyons@users.noreply.github.com>
2025-11-07 22:23:11 -0500

    backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,16 @@
 mod handlers;
 
 use actix_web::{App, HttpServer, web};
-use sqlx::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server is running on port http://localhost:8080");
 
-    let db = PgPool::connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-        .await
-        .expect("Failed to connect to database");
-
     HttpServer::new(move || {
-        App::new().app_data(web::Data::new(db.clone())).service(
-            web::scope("/api")
-                .route("/parcels", web::get().to(handlers::get_parcels))
-                .route("/parcels", web::post().to(handlers::create_parcel)),
-        )
+        App::new().service(web::scope("/api").route(
+            "/process-geo-data",
+            web::post().to(handlers::process_geo_data),
+        ))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
