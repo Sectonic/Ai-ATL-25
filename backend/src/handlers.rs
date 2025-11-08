7855ea66dc1708563834af146bd0b62fb8fe1241
Jamal <43446907+JamalLyons@users.noreply.github.com>
2025-11-07 22:28:22 -0500

    Backend

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -2,12 +2,12 @@ use actix_web::{HttpResponse, Result, web};
 use serde::{Deserialize, Serialize};
 
 // Request body for POST /parcels endpoint
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct CreateProcessRequest {
-    pub data: serde_json::Value,
+    pub data: String,
 }
 
 // POST /procress
 pub async fn process_geo_data(body: web::Json<CreateProcessRequest>) -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok().body("Hello, world!"))
+    Ok(HttpResponse::Ok().json(&body.data))
 }
